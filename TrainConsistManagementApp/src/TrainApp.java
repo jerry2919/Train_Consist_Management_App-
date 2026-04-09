93fc0d47da7fd900f6180633348c8f67198ec1ea
jerry2919 <tonysharmila05@gmail.com>
2026-04-09 15:10:24 +0530

    uc2

--- TrainConsistManagementApp/src/TrainApp.java
+++ TrainConsistManagementApp/src/TrainApp.java
@@ -4,10 +4,30 @@ import java.util.List;
 public class TrainApp {
 
     public static void main(String[] args) {
-        System.out.println("=== Train Consist Management App ===");
 
-        List<String> trainConsist = new ArrayList<>();
+        System.out.println("UC2 - Add Passenger Bogies to Train");
+        System.out.println("===================================");
 
-        System.out.println("Initial bogie count: " + trainConsist.size());
+        List<String> passengerBogies = new ArrayList<>();
+
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+
+        System.out.println("\nAfter Adding Bogies:");
+        System.out.println("Passenger Bogies : " + passengerBogies);
+
+        passengerBogies.remove("AC Chair");
+
+        System.out.println("\nAfter Removing 'AC Chair':");
+        System.out.println("Passenger Bogies : " + passengerBogies);
+
+        System.out.println("\nChecking if 'Sleeper' exists:");
+        System.out.println("Contains Sleeper? : " + passengerBogies.contains("Sleeper"));
+
+        System.out.println("\nFinal Train Passenger Consist:");
+        System.out.println(passengerBogies);
+
+        System.out.println("\nUC2 operations completed successfully...");
     }
 }
