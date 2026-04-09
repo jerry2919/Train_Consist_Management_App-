e426d461d405366ef587932517a9a37727c81e8d
jerry2919 <tonysharmila05@gmail.com>
2026-04-09 15:50:20 +0530

    uc9

--- TrainConsistManagementApp/src/TrainApp.java
+++ TrainConsistManagementApp/src/TrainApp.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class TrainApp {
 
@@ -20,7 +19,7 @@ public class TrainApp {
 
     public static void main(String[] args) {
 
-        System.out.println("UC8 - Filter Passenger Bogies Using Streams");
+        System.out.println("UC10 - Count Total Seats in Train");
         System.out.println("===================================");
 
         List<Bogie> bogies = new ArrayList<>();
@@ -29,16 +28,17 @@ public class TrainApp {
         bogies.add(new Bogie("AC Chair", 60));
         bogies.add(new Bogie("First Class", 24));
 
-        List<Bogie> filtered = bogies.stream()
-                .filter(b -> b.capacity > 60)
-                .collect(Collectors.toList());
-
-        System.out.println("\nFiltered Bogies (Capacity > 60):");
-
-        for (Bogie b : filtered) {
+        System.out.println("\nBogies in Train:");
+        for (Bogie b : bogies) {
             System.out.println(b);
         }
 
-        System.out.println("\nUC8 operations completed successfully...");
+        int total = bogies.stream()
+                .map(b -> b.capacity)
+                .reduce(0, Integer::sum);
+
+        System.out.println("\nTotal Seating Capacity: " + total);
+
+        System.out.println("\nUC10 operations completed successfully...");
     }
 }
